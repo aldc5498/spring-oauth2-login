33f72e8361b7cbb0021366816109efbeb9e0d6ee
alish <alish.dhamala@varosatech.co>
2022-07-09 15:58:50 +0545

    initial commit

---
+++ src/main/java/com/spring/oauth2/SpringOuath2DemoApplication.java
@@ -0,0 +1,13 @@
+package com.spring.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOuath2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOuath2DemoApplication.class, args);
+	}
+
+}
